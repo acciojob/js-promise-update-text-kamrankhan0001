a33dcdaba4b870c8f40a3bb0a960ef9de645870f
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-06 02:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+const outputElement = document.querySelector('#output');
+
+sayHello()
+  .then((message) => {
+    outputElement.textContent = message;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
